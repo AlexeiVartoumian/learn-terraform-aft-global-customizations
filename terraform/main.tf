ffa819c98347095b85669a43d5d4db7f4c6237bb
AlexeiVartoumian <alex.vartoumian@gmail.com>
2024-08-03 19:00:10 +0100

    see what happens

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,33 @@ resource "aws_s3_account_public_access_block" "block_all_public"{
     ignore_public_acls = true
     restrict_public_buckets = true
 }
+
+resource "aws_sns_topic" "aft_notifications" {
+    name = "aft-global-notifications"
+
+    tags = {
+        Nmae="AFT Global notifications"
+        Environment = "Production"
+        Managed_By = "Terraform"
+    }
+}
+
+resource "aws_sns_topic_policy" "default" {
+    arn = aws_sns_topic.aft_notifications.arn
+
+    policy = jsonencode({
+        Version = "2021-10-17"
+        Id = "aft-sns-topic-policy"
+        Statement = [
+            {
+                Sid = "AllowPublishFromAFT"
+                Effect = "Allow"
+                Principal = {
+                    Service = "events.amazonaws.com"
+                }
+                Action = "SNS:Publish"
+                Resource = aws_sns_topic.aft_notification.arn
+            }
+        ]
+    })
+}
